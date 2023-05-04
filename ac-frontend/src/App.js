78819a1408b445b92a57e3f68e8f549cc7ecc73d
Sofya <murz.sophie@gmail.com>
2023-05-04 20:23:13 +0200

    small changes in App.js

--- ac-frontend/src/App.js
+++ ac-frontend/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import axios from "axios";
+
+const baseURL = "http://localhost:8080/auth/";
+/* "start": "set PORT=3001 && react-scripts start", */
 
 function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Message from ArtConnect Backend:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{post}</p>
       </header>
     </div>
   );
