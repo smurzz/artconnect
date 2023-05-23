e0e3bc47ceabdfd815377c637ca7af78d8428456
KomolaBZR <komolabenzinger@gmail.com>
2023-05-23 23:00:46 +0200

    corrected a repository's package

--- ac-backend/src/test/java/com/artconnect/backend/repository/UserRepositoryTests.java
+++ ac-backend/src/test/java/com/artconnect/backend/repository/UserRepositoryTests.java
@@ -1,42 +1,40 @@
 package com.artconnect.backend.repository;
 
 
+
 import com.artconnect.backend.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.ExecutableUpdateOperation.UpdateWithQuery;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
-@DataMongoTest
-public class UserRepositoryTests {
-    @Autowired
+class UserRepositoryTests {
+
+    @Mock
     private UserRepository userRepository;
 
     @BeforeEach
-    public void setup() {
-        // Delete all documents from the collection
-        userRepository.deleteAll().block();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        // Delete all documents from the collection
-        userRepository.deleteAll().block();
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void testFindByEmail() {
+    public void testFindByEmailSuccess() {
         // Given
         String email = "test@example.com";
-        User user = new User();
-        user.setEmail(email);
-        userRepository.save(user).block();
+        User user = User.builder().email(email).build();
+        when(userRepository.findByEmail(email)).thenReturn(Mono.just(user));
 
         // When
         Mono<User> foundUserMono = userRepository.findByEmail(email);
@@ -49,24 +47,70 @@ public class UserRepositoryTests {
     }
 
     @Test
-    public void testFindByIsEnabled() {
+    public void testFindByEmailError() {
         // Given
-        User user1 = new User();
-        user1.setEnabled(true);
-        user1.setEmail("user1@example.com"); // Set a valid email
-        userRepository.save(user1).block();
-
-        User user2 = new User();
-        user2.setEnabled(false);
-        user2.setEmail("user2@example.com"); // Set a valid email
-        userRepository.save(user2).block();
+        String email = "nouser@example.com";
+        when(userRepository.findByEmail(email)).thenReturn(Mono.empty());
 
         // When
-        Flux<User> foundUsersFlux = userRepository.findByisEnabled(true);
+        Mono<User> foundUserMono = userRepository.findByEmail(email);
+
+        // Then
+        StepVerifier.create(foundUserMono)
+                .expectNextCount(0)
+                .expectComplete()
+                .verify();
+    }
+    @Test
+    public void testFindByIsEnabled_Success() {
+        // Given
+        boolean isEnabled = true;
+        User user1 = User.builder().isEnabled(isEnabled).email("user1@example.com").build();
+        User user2 = User.builder().isEnabled(isEnabled).email("user2@example.com").build();
+
+        when(userRepository.findByisEnabled(isEnabled)).thenReturn(Flux.just(user1, user2));
+
+        // When
+        Flux<User> foundUsersFlux = userRepository.findByisEnabled(isEnabled);
 
         // Then
         StepVerifier.create(foundUsersFlux)
-                .expectNextMatches(foundUser -> foundUser.isEnabled())
+                .expectNext(user1)
+                .expectNext(user2)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void testFindByIsEnabled_NoUsersFound() {
+        // Given
+        boolean isEnabled = true;
+
+        when(userRepository.findByisEnabled(isEnabled)).thenReturn(Flux.empty());
+
+        // When
+        Flux<User> foundUsersFlux = userRepository.findByisEnabled(isEnabled);
+
+        // Then
+        StepVerifier.create(foundUsersFlux)
+                .expectNextCount(0)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void testFindByIsEnabled_EmptyResult() {
+        // Given
+        boolean isEnabled = false;
+
+        when(userRepository.findByisEnabled(isEnabled)).thenReturn(Flux.empty());
+
+        // When
+        Flux<User> foundUsersFlux = userRepository.findByisEnabled(isEnabled);
+
+        // Then
+        StepVerifier.create(foundUsersFlux)
+                .expectNextCount(0)
                 .expectComplete()
                 .verify();
     }
