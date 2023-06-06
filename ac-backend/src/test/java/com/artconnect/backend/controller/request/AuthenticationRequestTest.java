6334a8e2fcbd36cac67150d7507e9abd56e9a021
KomolaBZR <komolabenzinger@gmail.com>
2023-06-06 21:08:34 +0200

    added more tests in AuthenticationRequestTest and RegisterRequestTest

--- ac-backend/src/test/java/com/artconnect/backend/controller/request/AuthenticationRequestTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/controller/request/AuthenticationRequestTest.java
@@ -119,4 +119,118 @@ public class AuthenticationRequestTest {
         Assertions.assertEquals(expectedToString, authenticationRequest.toString());
     }
 
+    @Test
+    public void testToString() {
+        // Arrange
+        String email = "test@example.com";
+        String password = "password";
+        AuthenticationRequest request = new AuthenticationRequest();
+        request.setEmail(email);
+        request.setPassword(password);
+
+        // Act
+        String toString = request.toString();
+
+        // Assert
+        Assertions.assertTrue(toString.contains("email=" + email));
+        Assertions.assertTrue(toString.contains("password=" + password));
+    }
+
+    @Test
+    public void testEquals() {
+        // Arrange
+        AuthenticationRequest request1 = new AuthenticationRequest();
+        request1.setEmail("test@example.com");
+        request1.setPassword("password");
+
+        AuthenticationRequest request2 = new AuthenticationRequest();
+        request2.setEmail("test@example.com");
+        request2.setPassword("password");
+
+        AuthenticationRequest request3 = new AuthenticationRequest();
+        request3.setEmail("test2@example.com");
+        request3.setPassword("password");
+
+        // Assert
+        Assertions.assertEquals(request1, request2); // Objects with the same values should be equal
+        Assertions.assertNotEquals(request1, request3); // Objects with different values should not be equal
+        Assertions.assertNotEquals(request2, null); // Object should not be equal to null
+    }
+
+    @Test
+    public void testNoArgsConstructor() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+
+        // Verify that all fields are initialized to their default values
+        Assertions.assertNull(authenticationRequest.getEmail());
+        Assertions.assertNull(authenticationRequest.getPassword());
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        String email = "test@example.com";
+        String password = "password123";
+
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(email, password);
+
+        // Verify that the fields are initialized with the provided values
+        Assertions.assertEquals(email, authenticationRequest.getEmail());
+        Assertions.assertEquals(password, authenticationRequest.getPassword());
+    }
+
+    @Test
+    public void testBuilder() {
+        String email = "test@example.com";
+        String password = "password123";
+
+        AuthenticationRequest authenticationRequest = AuthenticationRequest.builder()
+                .email(email)
+                .password(password)
+                .build();
+
+        // Verify that the fields are initialized with the provided values using the builder
+        Assertions.assertEquals(email, authenticationRequest.getEmail());
+        Assertions.assertEquals(password, authenticationRequest.getPassword());
+    }
+
+    @Test
+    public void testSetterWithNonNullValue() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        String email = "test@example.com";
+
+        authenticationRequest.setEmail(email);
+
+        // Verify that the email field is set to the provided value
+        Assertions.assertEquals(email, authenticationRequest.getEmail());
+    }
+
+    @Test
+    public void testSetterWithNullValue() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        String password = "password123";
+
+        authenticationRequest.setPassword(password);
+
+        // Verify that the password field is set to the provided value
+        Assertions.assertEquals(password, authenticationRequest.getPassword());
+    }
+
+    @Test
+    public void testGetterWithNonNullValue() {
+        String email = "test@example.com";
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(email, "password123");
+
+        // Verify that the getEmail() method returns the correct value
+        Assertions.assertEquals(email, authenticationRequest.getEmail());
+    }
+
+    @Test
+    public void testGetterWithNullValue() {
+        String password = "password123";
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest("test@example.com", password);
+
+        // Verify that the getPassword() method returns the correct value
+        Assertions.assertEquals(password, authenticationRequest.getPassword());
+    }
+
 }
