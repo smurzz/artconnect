4b6459a336acd20560931444ebb004a8a292e82d
KomolaBZR <komolabenzinger@gmail.com>
2023-06-06 17:03:30 +0200

    added more tests in AuthenticationRequestTest

--- ac-backend/src/test/java/com/artconnect/backend/controller/request/AuthenticationRequestTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/controller/request/AuthenticationRequestTest.java
@@ -54,4 +54,69 @@ public class AuthenticationRequestTest {
 
         Assertions.assertEquals("password123", authenticationRequest.getPassword());
     }
+
+    @Test
+    public void testDataMethods() {
+        String email = "test@example.com";
+        String password = "password123";
+
+        AuthenticationRequest authenticationRequest1 = new AuthenticationRequest(email, password);
+        AuthenticationRequest authenticationRequest2 = new AuthenticationRequest(email, password);
+
+        // Test equals() method
+        Assertions.assertEquals(authenticationRequest1, authenticationRequest2);
+
+        // Test hashCode() method
+        Assertions.assertEquals(authenticationRequest1.hashCode(), authenticationRequest2.hashCode());
+
+        // Test toString() method
+        String expectedToString = "AuthenticationRequest(email=test@example.com, password=password123)";
+        Assertions.assertEquals(expectedToString, authenticationRequest1.toString());
+    }
+
+    @Test
+    public void testEqualsWithDifferentObjects() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest("test@example.com", "password123");
+        String differentObject = "This is a different object";
+
+        Assertions.assertNotEquals(authenticationRequest, differentObject);
+    }
+
+    @Test
+    public void testEqualsWithNull() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest("test@example.com", "password123");
+
+        Assertions.assertNotEquals(authenticationRequest, null);
+    }
+
+    @Test
+    public void testHashCodeSameObject() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest("test@example.com", "password123");
+
+        Assertions.assertEquals(authenticationRequest.hashCode(), authenticationRequest.hashCode());
+    }
+    @Test
+    public void testHashCodeDifferentObjects() {
+        AuthenticationRequest authenticationRequest1 = new AuthenticationRequest("test@example.com", "password123");
+        AuthenticationRequest authenticationRequest2 = new AuthenticationRequest("test@example.com", "password123");
+
+        Assertions.assertEquals(authenticationRequest1.hashCode(), authenticationRequest2.hashCode());
+    }
+
+    @Test
+    public void testToStringWithPopulatedObject() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest("test@example.com", "password123");
+
+        String expectedToString = "AuthenticationRequest(email=test@example.com, password=password123)";
+        Assertions.assertEquals(expectedToString, authenticationRequest.toString());
+    }
+
+    @Test
+    public void testToStringWithEmptyObject() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+
+        String expectedToString = "AuthenticationRequest(email=null, password=null)";
+        Assertions.assertEquals(expectedToString, authenticationRequest.toString());
+    }
+
 }
