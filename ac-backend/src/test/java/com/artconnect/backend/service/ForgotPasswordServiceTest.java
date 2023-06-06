b338c8f3d42532906e04e41ec3eee702bd0094a0
KomolaBZR <komolabenzinger@gmail.com>
2023-06-06 13:47:01 +0200

    added one more test in  ForgotPasswordServiceTest

--- ac-backend/src/test/java/com/artconnect/backend/service/ForgotPasswordServiceTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/service/ForgotPasswordServiceTest.java
@@ -7,13 +7,12 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 import com.artconnect.backend.controller.request.ResetPasswordRequest;
-import jakarta.validation.constraints.Size;
-import lombok.*;
-import org.hibernate.validator.constraints.NotBlank;
+import jakarta.mail.MessagingException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,6 +21,13 @@ import com.artconnect.backend.model.User;
 import com.artconnect.backend.repository.UserRepository;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
 
 
 
@@ -149,6 +155,98 @@ class ForgotPasswordServiceTest {
         verifyNoInteractions(userRepository, jwtService, emailService);
     }
 
+
+
+
+    @Test
+    public void testProcessForgotPassword_EncodingError() {
+        // Arrange
+        String email = "example@example.com";
+        String subject = "Reset Password";
+        String content = "<table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "  <tbody><tr>\n" +
+                "    <td></td>\n" +
+                "    <td width=\"580\" valign=\"top\">\n" +
+                "      \n" +
+                "              <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                <tbody><tr>\n" +
+                "                  <td colspan=\"2\" height=\"20\"></td>\n" +
+                "                </tr>\n" +
+                "                <tr>\n" +
+                "                  <td width=\"30\"></td>\n" +
+                "                  <td align=\"left\">\n" +
+                "                    <div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;line-height:24px;color:#0b0c0c\">\n" +
+                "                      <p style=\"Margin:0\">If you're having trouble with the button above, you can also reset your password by clicking the link below:</p>\n" +
+                "                      <p style=\"Margin:0\"><a href=\"#\" style=\"color:#1D70B8\">Reset Password</a></p>\n" +
+                "                    </div>\n" +
+                "                  </td>\n" +
+                "                  <td width=\"30\"></td>\n" +
+                "                </tr>\n" +
+                "                <tr>\n" +
+                "                  <td colspan=\"2\" height=\"20\"></td>\n" +
+                "                </tr>\n" +
+                "              </tbody></table>\n" +
+                "      \n" +
+                "    </td>\n" +
+                "    <td></td>\n" +
+                "  </tr>\n" +
+                "</tbody></table>\n" +
+                "<table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "  <tbody><tr>\n" +
+                "    <td height=\"30\"></td>\n" +
+                "  </tr>\n" +
+                "  <tr>\n" +
+                "    <td width=\"10\" valign=\"middle\"></td>\n" +
+                "    <td>\n" +
+                "      \n" +
+                "              <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                <tbody><tr>\n" +
+                "                  <td bgcolor=\"#ffffff\" width=\"100%\" height=\"10\"></td>\n" +
+                "                </tr>\n" +
+                "              </tbody></table>\n" +
+                "      \n" +
+                "    </td>\n" +
+                "    <td width=\"10\" valign=\"middle\"></td>\n" +
+                "  </tr>\n" +
+                "</tbody></table>\n" +
+                "<table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "  <tbody><tr>\n" +
+                "    <td></td>\n" +
+                "    <td width=\"580\" valign=\"top\">\n" +
+                "      \n" +
+                "              <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                <tbody><tr>\n" +
+                "                  <td colspan=\"2\" height=\"20\"></td>\n" +
+                "                </tr>\n" +
+                "                <tr>\n" +
+                "                  <td width=\"30\"></td>\n" +
+                "                  <td align=\"left\">\n" +
+                "                    <div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;line-height:24px;color:#0b0c0c\">\n" +
+                "                      <p style=\"Margin:0;font-size:14px;line-height:18px\">If you have any questions, feel free to contact us at <a href=\"mailto:support@example.com\" style=\"color:#1D70B8;text-decoration:underline\">support@example.com</a>.</p>\n" +
+                "                    </div>\n" +
+                "                  </td>\n" +
+                "                  <td width=\"30\"></td>\n" +
+                "                </tr>\n" +
+                "                <tr>\n" +
+                "                  <td colspan=\"2\" height=\"20\"></td>\n" +
+                "                </tr>\n" +
+                "              </tbody></table>\n" +
+                "      \n" +
+                "    </td>\n" +
+                "    <td></td>\n" +
+                "  </tr>\n" +
+                "</tbody></table>\n" +
+                "\n" +
+                "<div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                "\n" +
+                "</div></div>";
+
+        // Act
+        emailService.sendEmail(email, subject, content);
+
+        // Assert
+        assertTrue(true); // Add your assertion here
+    }
 
 }
 
