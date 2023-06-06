017648a06c34775881aa4b596233de5119cca7f9
KomolaBZR <komolabenzinger@gmail.com>
2023-06-06 16:26:32 +0200

    added more tests in RegisterRequestTest

--- ac-backend/src/test/java/com/artconnect/backend/controller/request/RegisterRequestTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/controller/request/RegisterRequestTest.java
@@ -65,4 +65,77 @@ public class RegisterRequestTest {
     }
 
 
+    @Test
+    public void testRegisterRequestSetter() {
+        RegisterRequest registerRequest = new RegisterRequest();
+        registerRequest.setFirstname("John");
+        registerRequest.setLastname("Doe");
+        registerRequest.setEmail("john.doe@example.com");
+        registerRequest.setPassword("password123");
+
+        Assertions.assertEquals("John", registerRequest.getFirstname());
+        Assertions.assertEquals("Doe", registerRequest.getLastname());
+        Assertions.assertEquals("john.doe@example.com", registerRequest.getEmail());
+        Assertions.assertEquals("password123", registerRequest.getPassword());
+    }
+
+    @Test
+    public void testRegisterRequestEquals() {
+        RegisterRequest request1 = RegisterRequest.builder()
+                .firstname("John")
+                .lastname("Doe")
+                .email("john.doe@example.com")
+                .password("password123")
+                .build();
+
+        RegisterRequest request2 = RegisterRequest.builder()
+                .firstname("John")
+                .lastname("Doe")
+                .email("john.doe@example.com")
+                .password("password123")
+                .build();
+
+        Assertions.assertEquals(request1, request2);
+    }
+
+    @Test
+    public void testRegisterRequestHashCode() {
+        RegisterRequest request1 = RegisterRequest.builder()
+                .firstname("John")
+                .lastname("Doe")
+                .email("john.doe@example.com")
+                .password("password123")
+                .build();
+
+        RegisterRequest request2 = RegisterRequest.builder()
+                .firstname("John")
+                .lastname("Doe")
+                .email("john.doe@example.com")
+                .password("password123")
+                .build();
+
+        Assertions.assertEquals(request1.hashCode(), request2.hashCode());
+    }
+    @Test
+    public void testRegisterRequestToString() {
+        RegisterRequest registerRequest = RegisterRequest.builder()
+                .firstname("John")
+                .lastname("Doe")
+                .email("john.doe@example.com")
+                .password("password123")
+                .build();
+
+        String expectedToString = "RegisterRequest(firstname=John, lastname=Doe, email=john.doe@example.com, password=password123)";
+        Assertions.assertEquals(expectedToString, registerRequest.toString());
+    }
+
+    @Test
+    public void testRegisterRequestNoArgsConstructor() {
+        RegisterRequest registerRequest = new RegisterRequest();
+
+        Assertions.assertNull(registerRequest.getFirstname());
+        Assertions.assertNull(registerRequest.getLastname());
+        Assertions.assertNull(registerRequest.getEmail());
+        Assertions.assertNull(registerRequest.getPassword());
+    }
 }
