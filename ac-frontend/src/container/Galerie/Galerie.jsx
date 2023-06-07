b06cc61f107644e6c805b5d30fb1c64a64920d30
Monab3 <monabecher@gmx.de>
2023-06-08 00:11:35 +0200

    Bilder anpassen

--- ac-frontend/src/container/Galerie/Galerie.jsx
+++ ac-frontend/src/container/Galerie/Galerie.jsx
@@ -12,34 +12,47 @@ const Galerie = () => {
     const GalleryImages = [
         {
             // img: Images.dachbaustoffe
-            img: "https://oli-baustoffe-images.s3.eu-central-1.amazonaws.com/Olibaustoffe_bilder/DJI_0005.JPG",
+            img: "img\\Rectangleabstract.png",
             width: 180,
             height: 100,
+            titel: "Capturing the Essence",
+            text: "With each brushstroke, I danced between illumination and obscurity, seeking to unveil the hidden depths of existence."
         },
         {
             img: "https://oli-baustoffe-images.s3.eu-central-1.amazonaws.com/Olibaustoffe_bilder/DJI_0006.JPG",
             width: 180,
             height: 100,
+            titel: "Embracing Colors",
+            text: "Splashing pigments onto the canvas, I immersed myself in a symphony of hues, allowing the colors to sing harmoniously and ignite joy."
         },
         {
             img: "https://oli-baustoffe-images.s3.eu-central-1.amazonaws.com/Olibaustoffe_bilder/DJI_0007.JPG",
             width: 180,
             height: 100,
+            titel: "Beyond the Surface",
+            text: "Peering beyond the external facade, I delved into the soul's intricate labyrinth, painting the intangible emotions that lie beneath."
+
         },
         {
             img: "https://oli-baustoffe-images.s3.eu-central-1.amazonaws.com/Olibaustoffe_bilder/DJI_0008.JPG",
             width: 180,
             height: 100,
+            titel: "In Motion",
+            text: "As my hand raced across the canvas, I captured fleeting moments, immortalizing the raw emotions that pulsated through every stroke."
         },
         {
             img: "https://oli-baustoffe-images.s3.eu-central-1.amazonaws.com/Olibaustoffe_bilder/DJI_0013.JPG",
             width: 180,
             height: 100,
+            titel: "Whispers of Nature",
+            text: "Amidst tranquil landscapes, I breathed in the earth's whispers, translating its serenity onto the canvas, a testament to nature's eternal embrace."
         },
         {
             img: "https://oli-baustoffe-images.s3.eu-central-1.amazonaws.com/Olibaustoffe_bilder/DJI_0015.JPG",
             width: 180,
             height: 100,
+            titel: "The Human Canvas",
+            text: "Gazing into the eyes of my subjects, I beheld a mosaic of tales etched upon their faces, and with each stroke, I unraveled their untold narratives."
         },
     ]
 
@@ -89,11 +102,11 @@ const Galerie = () => {
                                 className='single card' 
                                 key={index}
                                 onClick={()=>handleOpenModal(index)}>
-                                <img src={slide.img} alt='test' class="card-img-top"  />
+                                <img src="imgSlides\matilda1.jpg" alt='test' class="card-img-top"  />
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    <h5 class="card-title">{slide.titel}</h5>
+                                    <p class="card-text">{slide.text}</p>
+                                    <a href="#" class="btn btn-primary">View</a>
                                 </div>
                             </div>
                         )
