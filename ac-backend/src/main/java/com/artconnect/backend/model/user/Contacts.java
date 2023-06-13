05defbaaba76ad1be0dbe2aac51ec8cfcf576cb2
Sofya <murz.sophie@gmail.com>
2023-06-13 14:22:13 +0200

    added attributes to User

---
+++ ac-backend/src/main/java/com/artconnect/backend/model/user/Contacts.java
@@ -0,0 +1,19 @@
+package com.artconnect.backend.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contacts {
+
+	private int telefonNumber;
+	
+    private Address address;
+    
+    private String website;
+}
