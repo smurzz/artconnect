d49dbcee68f5e0086ae64c5b0c441bfa8ebb02c1
KomolaBZR <komolabenzinger@gmail.com>
2023-06-16 23:23:39 +0200

    added more tests in the DimensionTest and ArtWorkTest

--- ac-backend/src/test/java/com/artconnect/backend/model/artwork/ArtWorkTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/model/artwork/ArtWorkTest.java
@@ -1,4 +1,85 @@
 package com.artconnect.backend.model.artwork;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.artconnect.backend.model.Image;
+import org.junit.jupiter.api.Test;
+
 public class ArtWorkTest {
+    @Test
+    public void testCreateArtWork() {
+        // Arrange
+        String title = "Test Artwork";
+        List<Image> images = new ArrayList<>();
+        String description = "Test artwork description";
+        int yearOfCreation = 2022;
+        int likes = 10;
+        List<String> materials = new ArrayList<>();
+        Dimension dimension = new Dimension();
+        double price = 99.99;
+        List<String> tags = new ArrayList<>();
+        String location = "Test location";
+        Date createdAt = new Date();
+
+        // Act
+        ArtWork artwork = new ArtWork();
+        artwork.setTitle(title);
+        artwork.setImages(images);
+        artwork.setDescription(description);
+        artwork.setYearOfCreation(yearOfCreation);
+        artwork.setLikes(likes);
+        artwork.setMaterials(materials);
+        artwork.setDimension(dimension);
+        artwork.setPrice(price);
+        artwork.setTags(tags);
+        artwork.setLocation(location);
+        artwork.setCreatedAt(createdAt);
+
+        // Assert
+        assertNotNull(artwork);
+        assertEquals(title, artwork.getTitle());
+        assertEquals(images, artwork.getImages());
+        assertEquals(description, artwork.getDescription());
+        assertEquals(yearOfCreation, artwork.getYearOfCreation());
+        assertEquals(likes, artwork.getLikes());
+        assertEquals(materials, artwork.getMaterials());
+        assertEquals(dimension, artwork.getDimension());
+        assertEquals(price, artwork.getPrice());
+        assertEquals(tags, artwork.getTags());
+        assertEquals(location, artwork.getLocation());
+        assertEquals(createdAt, artwork.getCreatedAt());
+    }
+
+    @Test
+    public void testUpdateArtworkTitle() {
+        // Arrange
+        ArtWork artwork = new ArtWork();
+        String newTitle = "Updated Title";
+
+        // Act
+        artwork.setTitle(newTitle);
+
+        // Assert
+        assertEquals(newTitle, artwork.getTitle());
+    }
+
+    @Test
+    public void testIncrementLikes() {
+        // Arrange
+        ArtWork artwork = new ArtWork();
+        int initialLikes = artwork.getLikes();
+        int expectedLikes = initialLikes + 1;
+
+        // Act
+        artwork.setLikes(artwork.getLikes() + 1);
+
+        // Assert
+        assertEquals(expectedLikes, artwork.getLikes());
+    }
+
 }
--- ac-backend/src/test/java/com/artconnect/backend/model/artwork/DimensionTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/model/artwork/DimensionTest.java
@@ -1,4 +1,105 @@
 package com.artconnect.backend.model.artwork;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DimensionTest {
+    private Dimension dimension;
+
+    @BeforeEach
+    public void setup() {
+        dimension = new Dimension();
+    }
+
+    @Test
+    public void testSetAndGetHeight() {
+        double expectedHeight = 10.0;
+        dimension.setHeight(expectedHeight);
+
+        double actualHeight = dimension.getHeight();
+
+        assertEquals(expectedHeight, actualHeight);
+    }
+
+    @Test
+    public void testSetAndGetWidth() {
+        double expectedWidth = 15.0;
+        dimension.setWidth(expectedWidth);
+
+        double actualWidth = dimension.getWidth();
+
+        assertEquals(expectedWidth, actualWidth);
+    }
+
+    @Test
+    public void testSetAndGetDepth() {
+        double expectedDepth = 5.0;
+        dimension.setDepth(expectedDepth);
+
+        double actualDepth = dimension.getDepth();
+
+        assertEquals(expectedDepth, actualDepth);
+    }
+
+    @Test
+    public void testBuilderConstructor() {
+        double expectedHeight = 10.0;
+        double expectedWidth = 15.0;
+        double expectedDepth = 5.0;
+
+        Dimension dimension = Dimension.builder()
+                .height(expectedHeight)
+                .width(expectedWidth)
+                .depth(expectedDepth)
+                .build();
+
+        assertEquals(expectedHeight, dimension.getHeight());
+        assertEquals(expectedWidth, dimension.getWidth());
+        assertEquals(expectedDepth, dimension.getDepth());
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        assertEquals(0.0, dimension.getHeight());
+        assertEquals(0.0, dimension.getWidth());
+        assertEquals(0.0, dimension.getDepth());
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        double expectedHeight = 10.0;
+        double expectedWidth = 15.0;
+        double expectedDepth = 5.0;
+
+        Dimension dimension = new Dimension(expectedHeight, expectedWidth, expectedDepth);
+
+        assertEquals(expectedHeight, dimension.getHeight());
+        assertEquals(expectedWidth, dimension.getWidth());
+        assertEquals(expectedDepth, dimension.getDepth());
+    }
+
+
+    @Test
+    public void testEqualsAndHashCode() {
+        Dimension dimension1 = new Dimension(10.0, 15.0, 5.0);
+        Dimension dimension2 = new Dimension(10.0, 15.0, 5.0);
+        Dimension dimension3 = new Dimension(5.0, 10.0, 15.0);
+
+        assertEquals(dimension1, dimension2);
+        assertNotEquals(dimension1, dimension3);
+
+        assertEquals(dimension1.hashCode(), dimension2.hashCode());
+        assertNotEquals(dimension1.hashCode(), dimension3.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        Dimension dimension = new Dimension(10.0, 15.0, 5.0);
+
+        String expectedToString = "Dimension(height=10.0, width=15.0, depth=5.0)";
+        assertEquals(expectedToString, dimension.toString());
+    }
 }
