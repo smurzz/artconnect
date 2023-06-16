beef39e56beebf924b38758670d3ce2ee3c855f1
KomolaBZR <komolabenzinger@gmail.com>
2023-06-16 21:15:00 +0200

    added more tests for the ContactsTest

--- ac-backend/src/test/java/com/artconnect/backend/model/user/ContactsTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/model/user/ContactsTest.java
@@ -7,8 +7,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class ContactsTest {
@@ -106,5 +105,96 @@ public class ContactsTest {
         assertEquals(123456789, contacts.getTelefonNumber());
         assertEquals(address, contacts.getAddress());
         assertEquals("www.example.com", contacts.getWebsite());
+    }
+
+    @Test
+    public void testNoArgsConstructor() {
+        Contacts emptyContacts = new Contacts();
+        assertEquals(0, emptyContacts.getTelefonNumber());
+        assertEquals(null, emptyContacts.getAddress());
+        assertEquals(null, emptyContacts.getWebsite());
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        // Create an instance of Address
+        Address address = Address.builder()
+                .street("123 Main St")
+                .postalCode("12345")
+                .city("City")
+                .country("Country")
+                .build();
+
+        // Create an instance of Contacts
+        Contacts allArgsContacts = new Contacts(123456789, address, "www.example.com");
+
+        // Verify the values
+        assertEquals(123456789, allArgsContacts.getTelefonNumber());
+        assertEquals(address, allArgsContacts.getAddress());
+        assertEquals("www.example.com", allArgsContacts.getWebsite());
+    }
+
+    @Test
+    public void testGettersAndSetters() {
+        // Test getters
+        assertEquals(123456789, contacts.getTelefonNumber());
+        assertEquals(address, contacts.getAddress());
+        assertEquals("www.example.com", contacts.getWebsite());
+
+        // Test setters
+        contacts.setTelefonNumber(987654321);
+        contacts.setAddress(null);
+        contacts.setWebsite("www.new-website.com");
+
+        assertEquals(987654321, contacts.getTelefonNumber());
+        assertEquals(null, contacts.getAddress());
+        assertEquals("www.new-website.com", contacts.getWebsite());
+    }
+
+    @Test
+    public void testEqualsAndHashCode() {
+        Contacts contacts1 = new Contacts(123456789, address, "www.example.com");
+        Contacts contacts2 = new Contacts(123456789, address, "www.example.com");
+        Contacts contacts3 = new Contacts(987654321, null, "www.new-website.com");
+
+        // Test equals
+        assertEquals(contacts1, contacts2);
+        assertNotEquals(contacts1, contacts3);
+
+        // Test hashCode
+        assertEquals(contacts1.hashCode(), contacts2.hashCode());
+        assertNotEquals(contacts1.hashCode(), contacts3.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        String expectedString = "Contacts(telefonNumber=123456789, address=Address(street=123 Main St, postalCode=12345, city=City, country=Country), website=www.example.com)";
+        assertEquals(expectedString, contacts.toString());
+    }
+
+    @Test
+    public void testBuilder() {
+        // Create an instance using the builder
+        Contacts builderContacts = Contacts.builder()
+                .telefonNumber(987654321)
+                .address(null)
+                .website("www.test.com")
+                .build();
+
+        // Verify the values
+        assertEquals(987654321, builderContacts.getTelefonNumber());
+        assertEquals(null, builderContacts.getAddress());
+        assertEquals("www.test.com", builderContacts.getWebsite());
+    }
+
+    @Test
+    public void testAllArgsConstructorWithDefaults() {
+        // Create an instance using the all-args constructor with default values
+        Contacts allArgsWithDefaultsContacts = new Contacts();
+
+        // Verify the default values
+        assertEquals(0, allArgsWithDefaultsContacts.getTelefonNumber());
+        assertEquals(null, allArgsWithDefaultsContacts.getAddress());
+        assertEquals(null, allArgsWithDefaultsContacts.getWebsite());
     }
 }
