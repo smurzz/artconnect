f94d114822b2d7d7d3159a89ca62c401f8ba3acd
KomolaBZR <komolabenzinger@gmail.com>
2023-06-17 23:18:43 +0200

    added more tests in the SocialMediaTest.java

--- ac-backend/src/test/java/com/artconnect/backend/model/user/SocialMediaTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/model/user/SocialMediaTest.java
@@ -229,4 +229,191 @@ public class SocialMediaTest {
         Assertions.assertEquals(socialMedia1.hashCode(), socialMedia2.hashCode());
     }
 
+    @Test
+    public void testSocialMediaSetTitleAndLink() {
+        // Create a SocialMedia object
+        SocialMedia socialMedia = new SocialMedia();
+
+        // Set the title and link using setters
+        socialMedia.setTitle("YouTube");
+        socialMedia.setLink("https://youtube.com");
+
+        // Verify the values using getters
+        Assertions.assertEquals("YouTube", socialMedia.getTitle());
+        Assertions.assertEquals("https://youtube.com", socialMedia.getLink());
+    }
+
+    @Test
+    public void testSocialMediaEqualsAndHashCodeWithNullValues() {
+        // Create two SocialMedia objects with null values
+        SocialMedia socialMedia1 = new SocialMedia(null, null);
+        SocialMedia socialMedia2 = new SocialMedia(null, null);
+
+        // Verify the equals() method with null values
+        Assertions.assertEquals(socialMedia1, socialMedia2);
+
+        // Verify the hashCode() method with null values
+        Assertions.assertEquals(socialMedia1.hashCode(), socialMedia2.hashCode());
+    }
+
+    @Test
+    public void testSocialMediaEqualsAndHashCodeWithEmptyValues() {
+        // Create two SocialMedia objects with empty values
+        SocialMedia socialMedia1 = new SocialMedia("", "");
+        SocialMedia socialMedia2 = new SocialMedia("", "");
+
+        // Verify the equals() method with empty values
+        Assertions.assertEquals(socialMedia1, socialMedia2);
+
+        // Verify the hashCode() method with empty values
+        Assertions.assertEquals(socialMedia1.hashCode(), socialMedia2.hashCode());
+    }
+
+    @Test
+    public void testSocialMediaNotEqualsWithDifferentTitle() {
+        // Create two SocialMedia objects with different titles
+        SocialMedia socialMedia1 = new SocialMedia("Twitter", "https://twitter.com");
+        SocialMedia socialMedia2 = new SocialMedia("Facebook", "https://facebook.com");
+
+        // Verify the objects are not equal
+        Assertions.assertNotEquals(socialMedia1, socialMedia2);
+    }
+
+    @Test
+    public void testSocialMediaNotEqualsWithDifferentLink() {
+        // Create two SocialMedia objects with different links
+        SocialMedia socialMedia1 = new SocialMedia("Twitter", "https://twitter.com");
+        SocialMedia socialMedia2 = new SocialMedia("Twitter", "https://facebook.com");
+
+        // Verify the objects are not equal
+        Assertions.assertNotEquals(socialMedia1, socialMedia2);
+    }
+
+    @Test
+    public void testSocialMediaNotEqualsWithNullTitle() {
+        // Create two SocialMedia objects with null title
+        SocialMedia socialMedia1 = new SocialMedia(null, "https://twitter.com");
+        SocialMedia socialMedia2 = new SocialMedia("Twitter", "https://twitter.com");
+
+        // Verify the objects are not equal
+        Assertions.assertNotEquals(socialMedia1, socialMedia2);
+    }
+
+    @Test
+    public void testSocialMediaNotEqualsWithNullLink() {
+        // Create two SocialMedia objects with null link
+        SocialMedia socialMedia1 = new SocialMedia("Twitter", null);
+        SocialMedia socialMedia2 = new SocialMedia("Twitter", "https://twitter.com");
+
+        // Verify the objects are not equal
+        Assertions.assertNotEquals(socialMedia1, socialMedia2);
+    }
+
+    @Test
+    public void testSocialMediaEqualsAndHashCodeWithSameObject() {
+        // Create a SocialMedia object
+        SocialMedia socialMedia = new SocialMedia("Twitter", "https://twitter.com");
+
+        // Verify the equals() method with the same object
+        Assertions.assertEquals(socialMedia, socialMedia);
+
+        // Verify the hashCode() method with the same object
+        Assertions.assertEquals(socialMedia.hashCode(), socialMedia.hashCode());
+    }
+
+    @Test
+    public void testSocialMediaEqualsAndHashCodeWithDifferentObjectClass() {
+        // Create a SocialMedia object
+        SocialMedia socialMedia = new SocialMedia("Twitter", "https://twitter.com");
+
+        // Verify the equals() method with a different object class
+        Assertions.assertNotEquals(socialMedia, new Object());
+
+        // Verify the hashCode() method with a different object class
+        Assertions.assertNotEquals(socialMedia.hashCode(), new Object().hashCode());
+    }
+
+    @Test
+    public void testSocialMediaToStringWithNonNullValues() {
+        // Create a SocialMedia object with non-null values
+        SocialMedia socialMedia = new SocialMedia("LinkedIn", "https://linkedin.com");
+
+        // Verify the toString() representation with non-null values
+        String expectedToString = "SocialMedia(title=LinkedIn, link=https://linkedin.com)";
+        Assertions.assertEquals(expectedToString, socialMedia.toString());
+    }
+
+    @Test
+    public void testSocialMediaSetTitleToNull() {
+        // Create a SocialMedia object
+        SocialMedia socialMedia = new SocialMedia("Twitter", "https://twitter.com");
+
+        // Set the title to null
+        socialMedia.setTitle(null);
+
+        // Verify that the title is null
+        Assertions.assertNull(socialMedia.getTitle());
+    }
+
+    @Test
+    public void testSocialMediaSetLinkToNull() {
+        // Create a SocialMedia object
+        SocialMedia socialMedia = new SocialMedia("Twitter", "https://twitter.com");
+
+        // Set the link to null
+        socialMedia.setLink(null);
+
+        // Verify that the link is null
+        Assertions.assertNull(socialMedia.getLink());
+    }
+
+    @Test
+    public void testSocialMediaEqualsAndHashCodeWithDifferentObjects() {
+        // Create two SocialMedia objects with different values
+        SocialMedia socialMedia1 = new SocialMedia("Twitter", "https://twitter.com");
+        SocialMedia socialMedia2 = new SocialMedia("Facebook", "https://facebook.com");
+
+        // Verify the equals() method with different objects
+        Assertions.assertNotEquals(socialMedia1, socialMedia2);
+
+        // Verify the hashCode() method with different objects
+        Assertions.assertNotEquals(socialMedia1.hashCode(), socialMedia2.hashCode());
+    }
+
+    @Test
+    public void testSocialMediaToStringWithEmptyValues() {
+        // Create a SocialMedia object with empty values
+        SocialMedia socialMedia = new SocialMedia("", "");
+
+        // Verify the toString() representation with empty values
+        String expectedToString = "SocialMedia(title=, link=)";
+        Assertions.assertEquals(expectedToString, socialMedia.toString());
+    }
+
+    @Test
+    public void testSocialMediaSetTitleAndLinkToEmpty() {
+        // Create a SocialMedia object
+        SocialMedia socialMedia = new SocialMedia("Twitter", "https://twitter.com");
+
+        // Set the title and link to empty
+        socialMedia.setTitle("");
+        socialMedia.setLink("");
+
+        // Verify that the title and link are empty
+        Assertions.assertEquals("", socialMedia.getTitle());
+        Assertions.assertEquals("", socialMedia.getLink());
+    }
+
+    @Test
+    public void testSocialMediaEqualsAndHashCodeWithNullAndEmptyValues() {
+        // Create two SocialMedia objects with null and empty values
+        SocialMedia socialMedia1 = new SocialMedia(null, null);
+        SocialMedia socialMedia2 = new SocialMedia("", "");
+
+        // Verify the equals() method with null and empty values
+        Assertions.assertNotEquals(socialMedia1, socialMedia2);
+
+        // Verify the hashCode() method with null and empty values
+        Assertions.assertNotEquals(socialMedia1.hashCode(), socialMedia2.hashCode());
+    }
 }
