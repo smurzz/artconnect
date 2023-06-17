0196a4eb9e427f7c12ab3883b6fbfb09c9f268ec
KomolaBZR <komolabenzinger@gmail.com>
2023-06-17 17:37:21 +0200

    added more tests in the ImageValidationTest

--- ac-backend/src/test/java/com/artconnect/backend/validation/ImageValidationTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/validation/ImageValidationTest.java
@@ -2,6 +2,7 @@ package com.artconnect.backend.validation;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.lang.reflect.Field;
 
 class ImageValidationTest {
 
@@ -124,5 +125,114 @@ class ImageValidationTest {
                 .build();
 
         Assertions.assertFalse(imageValidation.validFile());
+    }
+
+    @Test
+    void testGetterMethods() {
+        long expectedSize = 5242880;
+        String expectedContentType = "image/png";
+        String expectedFileName = "image.png";
+
+        ImageValidation imageValidation = ImageValidation.builder()
+                .size(expectedSize)
+                .contentType(expectedContentType)
+                .fileName(expectedFileName)
+                .build();
+
+        Assertions.assertEquals(expectedSize, imageValidation.getSize());
+        Assertions.assertEquals(expectedContentType, imageValidation.getContentType());
+        Assertions.assertEquals(expectedFileName, imageValidation.getFileName());
+    }
+
+    @Test
+    void testBuilderWithModifiedFields() {
+        long initialSize = 5242880;
+        String initialContentType = "image/png";
+        String initialFileName = "image.png";
+
+        long modifiedSize = 15728640;
+        String modifiedContentType = "image/jpeg";
+        String modifiedFileName = "picture.jpg";
+
+        ImageValidation imageValidation = ImageValidation.builder()
+                .size(initialSize)
+                .contentType(initialContentType)
+                .fileName(initialFileName)
+                .build();
+
+        ImageValidation modifiedValidation = ImageValidation.builder()
+                .size(modifiedSize)
+                .contentType(modifiedContentType)
+                .fileName(modifiedFileName)
+                .build();
+
+        Assertions.assertEquals(initialSize, imageValidation.getSize());
+        Assertions.assertEquals(initialContentType, imageValidation.getContentType());
+        Assertions.assertEquals(initialFileName, imageValidation.getFileName());
+
+        Assertions.assertEquals(modifiedSize, modifiedValidation.getSize());
+        Assertions.assertEquals(modifiedContentType, modifiedValidation.getContentType());
+        Assertions.assertEquals(modifiedFileName, modifiedValidation.getFileName());
+    }
+
+    @Test
+    void testToStringMethod() {
+        ImageValidation imageValidation = ImageValidation.builder()
+                .size(5242880)
+                .contentType("image/png")
+                .fileName("image.png")
+                .build();
+
+        String expected = "ImageValidation(size=5242880, contentType=image/png, fileName=image.png)";
+        String actual = imageValidation.toString();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testValidFileWithMaxSizeAndEmptyContentTypeAndFileName() {
+        ImageValidation imageValidation = ImageValidation.builder()
+                .size(10485760) // 10 MB
+                .contentType("")
+                .fileName("")
+                .build();
+
+        Assertions.assertFalse(imageValidation.validFile());
+    }
+
+    @Test
+    void testValidFileWithNegativeSize() {
+        ImageValidation imageValidation = ImageValidation.builder()
+                .size(-5242880)
+                .contentType("image/png")
+                .fileName("image.png")
+                .build();
+
+        Assertions.assertTrue(imageValidation.validFile());
+    }
+
+
+    @Test
+    void testValidFileWithEmptyContentType() {
+        ImageValidation imageValidation = ImageValidation.builder()
+                .size(5242880)
+                .contentType("")
+                .fileName("image.png")
+                .build();
+
+        Assertions.assertFalse(imageValidation.validFile());
+    }
+
+
+
+    @Test
+    void testValidFileWithMultipleDotsInFileName() {
+        ImageValidation imageValidation = ImageValidation.builder()
+                .size(5242880)
+                .contentType("image/png")
+                .fileName("image.test.png")
+                .build();
+
+        Assertions.assertTrue(imageValidation.validFile());
     }
 }
