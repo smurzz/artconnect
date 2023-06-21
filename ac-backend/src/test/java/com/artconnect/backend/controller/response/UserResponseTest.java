004dbd1d0f0c4ac925ec7fc889fedc01448bd473
KomolaBZR <komolabenzinger@gmail.com>
2023-06-21 22:21:19 +0200

    added some tests in the UserResponseTest.java

--- ac-backend/src/test/java/com/artconnect/backend/controller/response/UserResponseTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/controller/response/UserResponseTest.java
@@ -82,4 +82,73 @@ class UserResponseTest {
         // Assert that the UserResponse object's date of birth is null
         Assertions.assertNull(userResponse.getDateOfBirthday());
     }
+
+    @Test
+    void testNoArgsConstructor() {
+        UserResponse userResponse = new UserResponse();
+
+        // Assert that the UserResponse object is not null
+        Assertions.assertNotNull(userResponse);
+
+        // Assert that the fields in the UserResponse object are null
+        Assertions.assertNull(userResponse.getId());
+        Assertions.assertNull(userResponse.getFirstname());
+        Assertions.assertNull(userResponse.getLastname());
+        Assertions.assertNull(userResponse.getDateOfBirthday());
+        Assertions.assertNull(userResponse.getCreatedAt());
+        Assertions.assertNull(userResponse.getProfilePhoto());
+        Assertions.assertNull(userResponse.getBiography());
+        Assertions.assertNull(userResponse.getExhibitions());
+        Assertions.assertNull(userResponse.getContacts());
+        Assertions.assertNull(userResponse.getSocialMedias());
+    }
+
+    @Test
+    void testSetterMethods() {
+        // Create a new UserResponse object
+        UserResponse userResponse = new UserResponse();
+
+        // Set values using the setter methods
+        userResponse.setId("123");
+        userResponse.setFirstname("John");
+        userResponse.setLastname("Doe");
+        userResponse.setDateOfBirthday(LocalDate.of(1990, 1, 1));
+        userResponse.setCreatedAt(new Date());
+        userResponse.setProfilePhoto(new Image());
+        userResponse.setBiography("Sample biography");
+        userResponse.setExhibitions(new ArrayList<>());
+        userResponse.setContacts(new Contacts());
+        userResponse.setSocialMedias(new ArrayList<>());
+
+        // Assert that the UserResponse object contains the correct values
+        Assertions.assertEquals("123", userResponse.getId());
+        Assertions.assertEquals("John", userResponse.getFirstname());
+        Assertions.assertEquals("Doe", userResponse.getLastname());
+        Assertions.assertEquals(LocalDate.of(1990, 1, 1), userResponse.getDateOfBirthday());
+        Assertions.assertNotNull(userResponse.getCreatedAt());
+        Assertions.assertNotNull(userResponse.getProfilePhoto());
+        Assertions.assertEquals("Sample biography", userResponse.getBiography());
+        Assertions.assertNotNull(userResponse.getExhibitions());
+        Assertions.assertNotNull(userResponse.getContacts());
+        Assertions.assertNotNull(userResponse.getSocialMedias());
+    }
+    @Test
+    void testEqualsAndHashCode() {
+        UserResponse userResponse1 = UserResponse.fromUser(user);
+        UserResponse userResponse2 = UserResponse.fromUser(user);
+
+        // Assert that two UserResponse objects created from the same User have the same hash code
+        Assertions.assertEquals(userResponse1.hashCode(), userResponse2.hashCode());
+
+        // Assert that the two UserResponse objects are equal
+        Assertions.assertEquals(userResponse1, userResponse2);
+    }
+
+    @Test
+    void testToString() {
+        UserResponse userResponse = UserResponse.fromUser(user);
+
+        // Assert that the UserResponse object has a non-null string representation
+        Assertions.assertNotNull(userResponse.toString());
+    }
 }
