3640682c4db7cf0134397a012eedd6c6c0f994f8
KomolaBZR <komolabenzinger@gmail.com>
2023-06-22 12:46:33 +0200

    added some tests in the ImageControllerTest.java

--- ac-backend/src/test/java/com/artconnect/backend/controller/ImageControllerTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/controller/ImageControllerTest.java
@@ -1,5 +1,6 @@
 package com.artconnect.backend.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -9,6 +10,7 @@ import com.artconnect.backend.model.Image;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
@@ -16,6 +18,10 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import com.artconnect.backend.service.ImageService;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.util.AssertionErrors.fail;
+import static org.mockito.Mockito.never;
 
 public class ImageControllerTest {
 
@@ -88,4 +94,62 @@ public class ImageControllerTest {
         });
     }
 
+    @Test
+    public void testGetPhotoNotFound() {
+        // Mock image ID
+        String id = "456";
+
+        // Mock empty image response (image not found)
+        when(imageService.getPhoto(id)).thenReturn(Mono.empty());
+
+        // Invoke controller method
+        Mono<ResponseEntity<byte[]>> result = imageController.getPhoto(id);
+
+        // Verify the error response
+        result.subscribe(responseEntity -> {
+            assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        });
+    }
+
+    @Test
+    public void testAddPhotoThrowsIOException() throws IOException {
+        // Mock file and file size
+        FilePart filePart = mock(FilePart.class);
+        Mono<FilePart> filePartMono = Mono.just(filePart);
+        Long fileSize = 1000L;
+
+        // Mock IOException from the service
+        when(imageService.addPhoto(filePartMono, fileSize)).thenReturn(Mono.error(new IOException("Failed to add photo.")));
+
+        // Invoke controller method
+        Mono<ResponseEntity<byte[]>> result = imageController.addPhoto(filePartMono, fileSize);
+
+        // Verify the error response
+        result.subscribe(responseEntity -> {
+            fail("Expected an IOException.");
+        }, error -> {
+            assertTrue(error instanceof IOException);
+            assertEquals("Failed to add photo.", error.getMessage());
+        });
+    }
+
+    @Test
+    public void testGetPhotoThrowsException() {
+        // Mock image ID
+        String id = "123";
+
+        // Mock RuntimeException from the service
+        when(imageService.getPhoto(id)).thenReturn(Mono.error(new RuntimeException("Failed to get photo.")));
+
+        // Invoke controller method
+        Mono<ResponseEntity<byte[]>> result = imageController.getPhoto(id);
+
+        // Verify the error response
+        result.subscribe(responseEntity -> {
+            fail("Expected a RuntimeException.");
+        }, error -> {
+            assertTrue(error instanceof RuntimeException);
+            assertEquals("Failed to get photo.", error.getMessage());
+        });
+    }
 }
