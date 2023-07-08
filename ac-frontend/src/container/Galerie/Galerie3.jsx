f56d5e417fe261db02adcc259d3b7d625a46b36f
Zac <zac.upshure@outlook.com>
2023-07-08 12:27:41 +0200

    delete user button

--- ac-frontend/src/container/Galerie/Galerie3.jsx
+++ ac-frontend/src/container/Galerie/Galerie3.jsx
@@ -1,8 +1,94 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
+import {useNavigate, Link} from "react-router-dom";
 import {GalerieApiService} from "../../lib/apiGalerie"
 
-const Galerie3 = () => {
+import Image1 from './imgSlides/original.jpg';
+import Image2 from './imgSlides/original2.jpg';
+import Image3 from './imgSlides/original3.jpg';
+import Image4 from './imgSlides/original4.jpg';
+import Image5 from './imgSlides/original5.jpg';
+import Image6 from './imgSlides/original6.jpg';
+import Image7 from './imgSlides/original7.jpg';
+import Image8 from './imgSlides/original8.jpg';
+import Image9 from './imgSlides/original9.jpg';
 
+const artworks = [
+    {
+      id: 1,
+      price: 20,
+      name: 'Olivia Montague',
+      href: '/galerie/DetailImage/1',
+      imageSrc: Image1,
+      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    },
+    {
+      id: 2,
+      price: 20,
+      name: 'Sebastian Wolfe',
+      href: '/galerie/DetailImage/2',
+      imageSrc: Image2,
+      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    },
+    {
+      id: 3,
+      price: 20,
+      name: 'Isabella Marchand',
+      href: '/galerie/DetailImage/3',
+      imageSrc: Image3,
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 4,
+      price: 20,
+      name: 'Alexander Hartmann',
+      href: '/galerie/DetailImage/4',
+      imageSrc: Image4,
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    {
+      id: 5,
+      price: 20,
+      name: 'Victoria Lefevre',
+      href: '/galerie/DetailImage/5',
+      imageSrc: Image5,
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    {
+      id: 6,
+      price: 20,
+      name: 'Gabriel Delacroix',
+      href: '/galerie/DetailImage/6',
+      imageSrc: Image6,
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    {
+      id: 7,
+      price: 20,
+      name: 'Sophia Davenport',
+      href: '/galerie/DetailImage/7',
+      imageSrc: Image7,
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    {
+      id: 8,
+      price: 20,
+      name: 'Lucas Beaumont',
+      href: '/galerie/DetailImage/8',
+      imageSrc: Image8,
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    {
+      id: 9,
+      price: 20,
+      name: 'Amelia Rousseau',
+      href: '/galerie/DetailImage/9',
+      imageSrc: Image9,
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    // More products...
+  ]
+
+const Galerie3 = () => {
     const [Galerie, setGalerie] = useState([]);
 
     useEffect(() => {
@@ -14,9 +100,28 @@ const Galerie3 = () => {
         getGaleryData();
 
     }, [])
+    
 
   return (
-    <div>Galerie3</div>
+    <div className='container'>
+                        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                            {artworks.map((artwork) => (
+                                <Link to={`/galerie/DetailImage/${artwork.id}`}>
+                                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                                        <img
+                                            src={artwork.imageSrc}
+                                            alt="{product.imageAlt}"
+                                            className="h-full w-full object-cover object-center group-hover:opacity-75"
+                                        />
+                                    </div>
+                                    <h3 className="link mt-4 text-sm text-gray-700">{artwork.name}</h3>
+                                    <p className="link mt-1 text-lg font-medium text-gray-900">{artwork.description}</p>
+                                    <p className="link mt-1 text-lg font-medium text-gray-900">{artwork.price} Euro</p>
+
+                                </Link>
+                            ))}
+                        </div>
+    </div>
   )
 }
 
--- ac-frontend/src/container/ProfilBearbeiten/bearbeiten.js
+++ ac-frontend/src/container/ProfilBearbeiten/bearbeiten.js
@@ -597,13 +597,23 @@ return (
     </form>
     </div>
     </div>
-        <button
-            onClick={() => {
-            navigate("/deleteUser",  { state: { userId: user.id}});
-        }}>
-            Profil löschen
-        </button>
-    {/* tailwind ui */}
+
+    <div className="bg-white shadow sm:rounded-lg mt-7 mb-7">
+      <div className="px-4 py-5 sm:p-6">
+        <h3 className="text-base font-semibold leading-6 text-gray-900">If you want to delete your account.</h3>
+        <div className="mt-2 max-w-xl text-sm text-gray-500">
+            <p>Be careful it cannot be restored</p>
+        </div>
+        <div className="mt-5">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            onClick={() => {navigate("/deleteUser",  { state: { userId: user.id}});}}>
+            Delete
+          </button>
+        </div>
+      </div>
+      </div>
     </>
 );
 }
