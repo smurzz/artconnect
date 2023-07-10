10809c659498d1f6bb00778ff3811d7dde741a11
Sofya <murz.sophie@gmail.com>
2023-07-10 03:14:10 +0200

    created Comment Controller

--- ac-backend/src/main/java/com/artconnect/backend/controller/CommentController.java
+++ ac-backend/src/main/java/com/artconnect/backend/controller/CommentController.java
@@ -1,5 +1,59 @@
 package com.artconnect.backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.artconnect.backend.controller.request.CommentRequest;
+import com.artconnect.backend.controller.response.ArtWorkResponse;
+import com.artconnect.backend.model.artwork.ArtWork;
+import com.artconnect.backend.service.ArtWorkService;
+import com.artconnect.backend.service.CommentService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/artworks/{id}/comments")
+@RequiredArgsConstructor
+@Validated
 public class CommentController {
+
+	private final CommentService commentService;
+	
+	private final ArtWorkService artWorkService;
+
+	@PostMapping
+	public Mono<ArtWorkResponse> createComment(
+			@PathVariable String id,
+			@RequestBody CommentRequest commentRequest) {
+		return commentService.create(id, commentRequest).flatMap(this::mapArtWorkToResponse);
+	}
+	
+	@PutMapping("/{commentId}")
+	public Mono<ArtWorkResponse> updateComment(
+			@PathVariable String id,
+			@PathVariable String commentId,
+			@RequestBody CommentRequest commentRequest) {
+		return commentService.update(id, commentId, commentRequest).flatMap(this::mapArtWorkToResponse);
+	}
+	
+	@DeleteMapping("/{commentId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Mono<Void> deleteComment(@PathVariable String id, @PathVariable String commentId) {
+	    return commentService.delete(id, commentId);
+	}
+	
+	private Mono<ArtWorkResponse> mapArtWorkToResponse(ArtWork artWork) {
+        return artWorkService.mapArtWorkToResponse(artWork);
+    }
+
 
 }
