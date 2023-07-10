38e389ff206a991671a051fcc8392661ae01aedb
KomolaBZR <komolabenzinger@gmail.com>
2023-07-10 15:01:12 +0200

    added some tests in NotBlankIfSpecifiedValidatorTest

---
+++ ac-backend/src/test/java/com/artconnect/backend/validation/NotBlankIfSpecifiedValidatorTest.java
@@ -0,0 +1,34 @@
+package com.artconnect.backend.validation;
+
+import jakarta.validation.ConstraintValidatorContext;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class NotBlankIfSpecifiedValidatorTest {
+
+    @Test
+    void testIsValidWithNullValue() {
+        NotBlankIfSpecifiedValidator validator = new NotBlankIfSpecifiedValidator();
+        assertTrue(validator.isValid(null, mock(ConstraintValidatorContext.class)));
+    }
+
+    @Test
+    void testIsValidWithEmptyValue() {
+        NotBlankIfSpecifiedValidator validator = new NotBlankIfSpecifiedValidator();
+        assertFalse(validator.isValid("", mock(ConstraintValidatorContext.class)));
+    }
+
+    @Test
+    void testIsValidWithBlankValue() {
+        NotBlankIfSpecifiedValidator validator = new NotBlankIfSpecifiedValidator();
+        assertFalse(validator.isValid("   ", mock(ConstraintValidatorContext.class)));
+    }
+
+    @Test
+    void testIsValidWithNonEmptyValue() {
+        NotBlankIfSpecifiedValidator validator = new NotBlankIfSpecifiedValidator();
+        assertTrue(validator.isValid("some value", mock(ConstraintValidatorContext.class)));
+    }
+
+}
