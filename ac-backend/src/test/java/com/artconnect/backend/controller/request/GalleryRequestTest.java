9b2242b331239b7fa1df34e8ee7d757e7ba4ebb7
KomolaBZR <komolabenzinger@gmail.com>
2023-07-11 13:04:03 +0200

    added tests in GalleryRequestTest

--- ac-backend/src/test/java/com/artconnect/backend/controller/request/GalleryRequestTest.java
+++ ac-backend/src/test/java/com/artconnect/backend/controller/request/GalleryRequestTest.java
@@ -11,6 +11,7 @@ import jakarta.validation.ValidatorFactory;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.*;
 public class GalleryRequestTest {
@@ -40,6 +41,37 @@ public class GalleryRequestTest {
 
         Set<ConstraintViolation<GalleryRequest>> violations = validator.validate(request);
 
-        Assertions.assertTrue(violations.isEmpty());
+        assertTrue(violations.isEmpty());
     }
+
+    @Test
+    void shouldPassValidationWithArtworkIds() {
+        GalleryRequest request = new GalleryRequest();
+        request.setTitle("Sample Title");
+
+        List<String> artworkIds = new ArrayList<>();
+        artworkIds.add("Artwork 1");
+        artworkIds.add("Artwork 2");
+        request.setArtworkIds(artworkIds);
+
+        Set<ConstraintViolation<GalleryRequest>> violations = validator.validate(request);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void shouldPassValidationWithEvaluations() {
+        GalleryRequest request = new GalleryRequest();
+        request.setTitle("Sample Title");
+
+        Map<String, Integer> evaluations = new HashMap<>();
+        evaluations.put("User1", 5);
+        evaluations.put("User2", 4);
+        request.setEvaluations(evaluations);
+
+        Set<ConstraintViolation<GalleryRequest>> violations = validator.validate(request);
+
+        assertTrue(violations.isEmpty());
+    }
+
 }
