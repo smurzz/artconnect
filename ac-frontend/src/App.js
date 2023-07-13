2a9e69ee56151e01972cf5bfc3cc030ecd464418
Sofya <murz.sophie@gmail.com>
2023-07-13 12:45:26 +0200

    init project

---
+++ ac-frontend/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import HomePublic from './react/pages/HomePublic';
+import HomePrivate from './react/pages/HomePrivate';
+import UsersPage from './react/pages/UsersPage';
+import Login from './react/pages/Login';
+import MyProfile from './react/pages/MyProfile';
+import Profile from './react/pages/Profile';
+import { Register } from './react/pages/Register';
+import { ForgotPassword } from './react/pages/ForgotPassword';
+import { ResetPassword } from './react/pages/ResetPassword';
+import PrivateRoute from './PrivateRoute';
+import { useDispatch, useSelector } from 'react-redux';
+
+import * as authActions from './redux/authentication/AuthenticationAction'; 
+import LoadingPage from './react/pages/components/LoadingPage';
+/* 
+import ForgotPassword from './react/pages/components/forgotPassword';
+import ResetPasswordForm from './react/pages/components/resetPasswordForm'; */
+
+function App() {
+  
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={ <HomePublic />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/forgot-password' element={< ForgotPassword/>} />
+        <Route exact path='/reset-password' element={< ResetPassword/>} />
+        <Route exact path='/home' element={< HomePrivate/>} />
+        <Route exact path='/profile-edit' element={< MyProfile/>} />
+        <Route exact path='/users' element={< UsersPage/>} />
+        <Route exact path='/user/:id' element={< Profile/>} />
+      
+        {/* <Route 
+          exact path="/home" 
+          element={authData.isAuthenticated ? <HomePrivate /> : <Navigate to="/login" replace />} /> */}
+        {/* 
+        <Route exact path='/forgot-password' element={< ForgotPassword/>} />
+        <Route exact path='/reset-password' element={< ResetPasswordForm/>} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ ac-frontend/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/RootReducer';
+
+const initialState = {};
+const middlewares = [thunk];
+
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middlewares)));
+axios.defaults.baseURL = 'http://localhost:8080/';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    < React.StrictMode >
+      <App />
+    </React.StrictMode >
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
