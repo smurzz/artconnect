370ae9343cf2a3f0a51c7cae939f426ea0de6150
Sofya <murz.sophie@gmail.com>
2023-07-15 20:32:08 +0200

    added mongo atlas config

--- ac-backend/src/main/java/com/artconnect/backend/AcBackendApplication.java
+++ ac-backend/src/main/java/com/artconnect/backend/AcBackendApplication.java
@@ -1,10 +1,21 @@
 package com.artconnect.backend;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
 
 @SpringBootApplication
 @EnableScheduling
@@ -13,6 +24,47 @@ public class AcBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcBackendApplication.class, args);
+	}
+	
+	@Configuration
+	@Profile("default")
+	static class LocalMongoDBConfiguration {
+		
+		@Value("${spring.data.mongodb.host}")
+		private String mongoHost;
+		
+		@Value("${spring.data.mongodb.port}")
+		private int mongoPort;
+		
+		@Value("${spring.data.mongodb.database}")
+		private String mongoDatabase;
+		
+		@Bean
+		MongoClientSettingsBuilderCustomizer mongoClientSettingsBuilderCustomizer() {
+			return settingsBuilder -> {
+				MongoClientSettings.Builder builder = settingsBuilder.applyToClusterSettings(clusterSettings -> {
+					clusterSettings.hosts(Arrays.asList(new ServerAddress(mongoHost, mongoPort)));
+				});
+				settingsBuilder.applyConnectionString(new ConnectionString("mongodb://" + mongoHost + ":" + mongoPort));
+				builder.retryWrites(true);
+			};
+		}
+	}
+	
+	@Configuration
+	@Profile("docker")
+	static class DockerMongoDBConfiguration {
+		
+		@Value("${spring.data.mongodb.uri}")
+		private String mongoUri;
+		
+		@Bean
+		MongoClientSettingsBuilderCustomizer mongoClientSettingsBuilderCustomizer() {
+			return settingsBuilder -> {
+				settingsBuilder.applyConnectionString(new ConnectionString(mongoUri));
+				settingsBuilder.retryWrites(true);
+			};
+		}
 	}
 
 }
